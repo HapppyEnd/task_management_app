2bc20eaefe3e0a1b0189e14ded38181b85a90f53
Alena <sweetysweetygirl@gmail.com>
2025-03-02 00:25:17 +0300

    Added nginx, prometheus, grafana config. Added routes reristration, login, JWT auth.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://taskuser:taskpassword@database:5432/taskdb'
+app.config['JWT_SECRET_KEY'] = 'secret_key'
 db = SQLAlchemy(app)
-
+jwt = JWTManager(app)
 from app import routes
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,17 +1,55 @@
 from app import app, db
 from app.models import User, Task
 from flask import request, jsonify
+from flask_jwt_extended import (create_access_token, jwt_required,
+                                get_jwt_identity)
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from prometheus_client import make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
+
 
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+    new_user = User(username=data.get('username'), password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User registered successfully'}), 201
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data.get('username')).first()
+    if not user or not check_password_hash(user.password,
+                                           data.get('password')):
+        return jsonify({'message': 'Invalid username or password'}), 401
+
+    access_token = create_access_token(identity=user.id)
+    return jsonify({'access_token': access_token}), 200
+
+
 @app.route('/tasks', methods=['GET'])
+@jwt_required()
 def get_tasks():
-    tasks = Task.query.all()
+    current_user = get_jwt_identity()
+    tasks = Task.query.filter_by(owner_id=current_user).all()
     return jsonify([task.todict() for task in tasks])
 
 
 @app.route('/tasks', methods=['POST'])
+@jwt_required()
 def create_task():
     data = request.get_json()
     new_task = Task(title=data.get('title'),
@@ -21,12 +59,16 @@ def create_task():
     db.session.commit()
     return jsonify(new_task.todict()), 201
 
+
 @app.route('/tasks/<int:id>', methods=['GET'])
+@jwt_required()
 def get_task(task_id):
     task = Task.query.get_or_404(task_id)
     return jsonify(task.todict())
 
+
 @app.route('/tasks/<int:id>', methods=['PUT'])
+@jwt_required()
 def update_task(task_id):
     data = request.get_json()
     task = Task.query.get_or_404(task_id)
@@ -37,7 +79,9 @@ def update_task(task_id):
     db.session.commit()
     return jsonify(task.todict())
 
+
 @app.route('/tasks/<int:id>', methods=['DELETE'])
+@jwt_required()
 def delete_task(task_id):
     task = Task.query.get_or_404(task_id)
     db.session.delete(task)
